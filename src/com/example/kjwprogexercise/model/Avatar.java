6156fc620749ae6749a747382425e70a443006eb
Kurt Williams <kwilliams@curriculumloft.com>
2014-11-06 17:03:47 -0500

    added code

---
+++ src/com/example/kjwprogexercise/model/Avatar.java
@@ -0,0 +1,7 @@
+package com.example.kjwprogexercise.model;
+
+public class Avatar {
+	public String src;
+	public Integer width;
+	public Integer height;
+}
---
+++ src/com/example/kjwprogexercise/model/Offer.java
@@ -0,0 +1,9 @@
+package com.example.kjwprogexercise.model;
+
+public class Offer {
+	public String attrib;
+	public String desc;
+	public String href;
+	public String src;
+	public User user;
+}
